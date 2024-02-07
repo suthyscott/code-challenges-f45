fcc5c8346b896574ab109f3d400bc2625afdc161
suthyscott <sutherlandscott21@gmail.com>
2024-02-07 09:28:18 -0700

    solved hasMoreVowels.js

--- week-3/hasMoreVowels.js
+++ week-3/hasMoreVowels.js
@@ -22,31 +22,32 @@ hasMoreVowels('Aal')
 // true
 */
 
-// const hasMoreVowels = word => {
-//     word = word.toLowerCase()
-//     const vowels = ['a', 'e', 'i', 'o', 'u']
-//     let vowelCount = 0
-//     let consonantCount = 0
+const hasMoreVowels = word => {
+    word = word.toLowerCase()
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let vowelCount = 0
+    let consonantCount = 0
 
-//     word.split('').forEach(letter => {
-//         if(vowels.includes(letter)){
-//             vowelCount++
-//         } else {
-//             consonantCount++
-//         }
-//     })
+    word.split('').forEach(letter => {
+        if(vowels.includes(letter)){
+            vowelCount++
+        } else {
+            consonantCount++
+        }
+    })
 
-//     if(vowelCount > consonantCount){
-//         return true
-//     } else {
-//         return false
-//     }
-// }
+    if(vowelCount > consonantCount){
+        return true
+    } else {
+        return false
+    }
+}
 
-const hasMoreVowelsReduce = word => word.toLowerCase().reduce((acc, curr, i) => {
-    ['a', 'e', 'i', 'o', 'u'].includes(curr) ? acc.vowelCount += 1 : acc.consonantCount += 1
-    return i === word.length - 1 ? acc.vowelCount > acc.consonantCount ? true : false : acc
-}, {consonants:0, vowels:0})
+// Reduce not working yet
+// const hasMoreVowelsReduce = word => word.toLowerCase().reduce((acc, curr, i) => {
+//     ['a', 'e', 'i', 'o', 'u'].includes(curr) ? acc.vowelCount += 1 : acc.consonantCount += 1
+//     return i === word.length - 1 ? acc.vowelCount > acc.consonantCount ? true : false : acc
+// }, {consonants:0, vowels:0})
 
 // const hasMoreVowels = word => word.toLowerCase().split('').reduce((acc, curr, i) => i === (word.length - 1) ? acc.vowels > acc.consonants ? true : false : ['a', 'e', 'i', 'o', 'u'].includes(curr) ? {...acc, vowels: acc.vowels + 1} : {...acc, consonants: acc.consonants + 1}, {consonants:0, vowels:0})
 
